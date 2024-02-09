b42f802697aa7a9c7032ed3038dc2ede81b6cd15
Ben Stickel <ben.stickel@elastic.co>
2024-02-08 23:07:04 -0800

    fix: buildkite testing
    Signed-off-by: Ben Stickel <ben.stickel@elastic.co>

--- .buildkite/scripts/plan.sh
+++ .buildkite/scripts/plan.sh
@@ -8,7 +8,7 @@ function main() {
   local WORKSPACE=${1}
   cd ${WORKSPACE}
   terraform init -get=true
-  terraform plan -detailed-exitcode -out
+  terraform plan -detailed-exitcode -out $(date -u +%m%dT%H%M%SZ).tfplan
 }
 
 main $TF_WORKSPACE
