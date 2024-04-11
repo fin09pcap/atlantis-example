259b1f304b31a831a99f14f273d0438f71dcd448
Ben Stickel <ben.stickel@elastic.co>
2024-04-11 00:07:55 -0700

    test: script update

--- .buildkite/scripts/vault-auth-lookup.sh
+++ .buildkite/scripts/vault-auth-lookup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-declare VAULT_SECRET_ID
-declare VAULT_ROLE_ID
-declare VAULT_TOKEN
-
-export VAULT_SECRET_ID=$(vault unwrap -field=secret_id $WRAPPED_SECRET_ID_TOKEN)
-export VAULT_ROLE_ID=2389919d-d3df-348a-3a89-406cf0821287
-export VAULT_TOKEN="$(vault write auth/approle/login -format=json role_id=${VAULT_ROLE_ID} secret_id=${VAULT_SECRET_ID} | jq .auth.client_token)"
+export VAULT_SECRET_ID="$(vault unwrap -field=secret_id $WRAPPED_SECRET_ID_TOKEN)"
+export VAULT_ROLE_ID="2389919d-d3df-348a-3a89-406cf0821287"
+export VAULT_TOKEN="$(vault write auth/approle/login -field=token role_id=${VAULT_ROLE_ID} secret_id=${VAULT_SECRET_ID})"
 export TF_VAR_vault_addr=$VAULT_ADDR
 
 echo "VAULT Login complete"
