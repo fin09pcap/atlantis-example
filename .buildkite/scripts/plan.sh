74a8fdad4b566c15e2328881dd71dc300a315734
Ben Stickel <ben.stickel@elastic.co>
2024-04-11 00:20:43 -0700

    test: update script arguments

--- .buildkite/scripts/plan.sh
+++ .buildkite/scripts/plan.sh
@@ -6,7 +6,7 @@ TF_WORKSPACE=${1:-}
 
 export VAULT_SECRET_ID="$(vault unwrap -field=secret_id $WRAPPED_SECRET_ID_TOKEN)"
 export VAULT_ROLE_ID="2389919d-d3df-348a-3a89-406cf0821287"
-export VAULT_TOKEN="$(vault write auth/approle/login -field=token role_id=${VAULT_ROLE_ID} secret_id=${VAULT_SECRET_ID})"
+export VAULT_TOKEN="$(vault write -field=token auth/approle/login role_id=${VAULT_ROLE_ID} secret_id=${VAULT_SECRET_ID})"
 
 function main() {
   local WORKSPACE=${1}
